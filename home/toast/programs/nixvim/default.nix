39fd29e8e5db81aec8feb29f34b78447aa4c0df6
lightly-toasted <tooast@duck.com>
2025-08-03 21:04:07 +0900

    feat(nixvim): add opts

--- home/toast/programs/nixvim/default.nix
+++ home/toast/programs/nixvim/default.nix
@@ -3,6 +3,14 @@
     enable = true;
     defaultEditor = true;
     colorschemes.ayu.enable = true;
+    opts = {
+      number = true;
+      relativenumber = true;
+      expandtab = true;
+      shiftwidth = 2;
+      tabstop = 2;
+      softtabstop = 2;
+    };
   };
 
   imports = [
