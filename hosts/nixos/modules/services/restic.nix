1f045e291d5cb5da73ac6d0816de020e87f2148a
lightly-toasted <tooast@duck.com>
2025-08-26 16:29:01 +0900

    feat: add restic

---
+++ hosts/nixos/modules/services/restic.nix
@@ -0,0 +1,27 @@
+{ config, ... }:
+
+{
+  sops.secrets."restic/password" = { };
+  sops.secrets."restic/rclone-config" = { };
+
+  services.restic.backups.gdrive = {
+    initialize = true;
+    inhibitsSleep = true;
+    passwordFile = config.sops.secrets."restic/password".path;
+    paths = [
+      "/data/Backup"
+      "/home/toast/workspace"
+    ];
+    repository = "rclone:gdrive/restic";
+    rcloneConfigFile = config.sops.secrets."restic/rclone-config".path;
+    pruneOpts = [
+      "--keep-daily 7"
+      "--keep-weekly 3"
+      "--keep-monthly 3"
+    ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+    };
+  };
+}
