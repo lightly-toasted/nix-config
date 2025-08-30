8a106e27583882879e00806c1153c5b86bfbbe0f
lightly-toasted <tooast@duck.com>
2025-08-30 12:59:50 +0900

    feat: migrate Prism Launcher from flatpak to home nix package

--- hosts/nixos/modules/services/flatpak.nix
+++ hosts/nixos/modules/services/flatpak.nix
@@ -4,17 +4,11 @@
   services.flatpak = {
     enable = true;
     packages = [
-      "org.prismlauncher.PrismLauncher"
       "org.vinegarhq.Sober"
       "org.vinegarhq.Vinegar"
       "com.spotify.Client"
     ];
     overrides = {
-      "org.prismlauncher.PrismLauncher".Context = {
-        filesystems = [
-          "home"
-        ];
-      };
       "org.vinegarhq.Sober".Context = {
         filesystems = [
           "xdg-run/app/com.discordapp.Discord:create"
