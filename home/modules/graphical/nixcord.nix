6c31c6e031e7aebc0888b602c5d7baafb5967fe9
lightly-toasted <tooast@duck.com>
2025-09-05 22:17:15 +0900

    fix(nixcord): switch to Discord client and remove spotifyControls plugin
    - Replaced Vesktop with the official Discord client as a workaround for crashes that occurred during screensharing.
    - Removed spotifyControls plugin due to buggy behavior
    - Could not solve Vencord plugins tab not being rendered

--- home/modules/graphical/nixcord.nix
+++ home/modules/graphical/nixcord.nix
@@ -1,33 +1,32 @@
 {
   programs.nixcord = {
     enable = true;
-    discord.enable = false;
-    vesktop.enable = true;
+    discord.enable = true;
+    vesktop.enable = false;
     config = {
       plugins = {
         biggerStreamPreview.enable = true;
-	callTimer.enable = true;
-	experiments.enable = true;
-	fixSpotifyEmbeds.enable = true;
-	fixYoutubeEmbeds.enable = true;
-	forceOwnerCrown.enable = true;
-	friendsSince.enable = true;
-	gameActivityToggle.enable = true;
-	permissionsViewer.enable = true;
-	serverInfo.enable = true;
-	silentTyping.enable = true;
-	spotifyControls.enable = true;
-	spotifyCrack.enable = true;
-	summaries.enable = true;
-	typingIndicator.enable = true;
-	validReply.enable = true;
-	validUser.enable = true;
-	voiceChatDoubleClick.enable = true;
-	voiceMessages.enable = true;
-	volumeBooster.enable = true;
-	webKeybinds.enable = true;
-	webScreenShareFixes.enable = true;
-	youtubeAdblock.enable = true;
+        callTimer.enable = true;
+        experiments.enable = true;
+        fixSpotifyEmbeds.enable = true;
+        fixYoutubeEmbeds.enable = true;
+        forceOwnerCrown.enable = true;
+        friendsSince.enable = true;
+        gameActivityToggle.enable = true;
+        permissionsViewer.enable = true;
+        serverInfo.enable = true;
+        silentTyping.enable = true;
+        spotifyCrack.enable = true;
+        summaries.enable = true;
+        typingIndicator.enable = true;
+        validReply.enable = true;
+        validUser.enable = true;
+        voiceChatDoubleClick.enable = true;
+        voiceMessages.enable = true;
+        volumeBooster.enable = true;
+        webKeybinds.enable = true;
+        webScreenShareFixes.enable = true;
+        youtubeAdblock.enable = true;
       };
     };
   };
