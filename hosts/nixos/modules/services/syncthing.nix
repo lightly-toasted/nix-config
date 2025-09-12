513ba7745897bf2690dcfb47586a33cd52103474
lightly-toasted <tooast@duck.com>
2025-09-12 23:13:08 +0900

    feat(nixos): declare syncthing settings

--- hosts/nixos/modules/services/syncthing.nix
+++ hosts/nixos/modules/services/syncthing.nix
@@ -1,6 +1,26 @@
 {
   services.syncthing = {
     enable = true;
+    user = "toast";
+    group = "users";
+    configDir = "/home/toast/.config/syncthing";
     extraFlags = [ "--no-default-folder" ];
+    overrideDevices = true;
+    overrideFolders = true;
+    settings = {
+      devices = {
+        "phone" = { id = "XIRT2YS-3PZY7PF-PL4QTYK-LCYCSKW-GARN5N6-KBWGSP6-YZDQGED-N7R6YQF"; };
+      };
+      folders = {
+        "keepass" = {
+          path = "/data/Backup/Keepass/";
+          devices = [ "phone" ];
+        };
+        "obsidian" = {
+          path = "/home/toast/Obsidian/";
+          devices = [ "phone" ];
+        };
+      };
+    };
   };
 }
