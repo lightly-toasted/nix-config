6e88dd69a822d762accf59848a70e72e517f21bc
lightly-toasted <tooast@duck.com>
2025-09-20 12:57:57 +0900

    feat(obsidian): switch to flatpak version

--- hosts/nixos/modules/services/flatpak.nix
+++ hosts/nixos/modules/services/flatpak.nix
@@ -7,12 +7,19 @@
       "org.vinegarhq.Sober"
       "org.vinegarhq.Vinegar"
       "com.spotify.Client"
+      "md.obsidian.Obsidian"
     ];
     overrides = {
       "org.vinegarhq.Sober".Context = {
         filesystems = [
           "xdg-run/app/com.discordapp.Discord:create"
           "xdg-run/discord-ipc-0"
+        ];
+      };
+      "md.obsidian.Obsidian".Context = {
+        filesystems = [
+          "home"
+          "/data/Backup/Obsidian"
         ];
       };
     };
