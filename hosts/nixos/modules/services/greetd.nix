2ade554397ccc95e6a83e29610cdce7555d0402d
lightly-toasted <tooast@duck.com>
2025-10-10 15:12:04 +0900

    chore(nixos): rename greetd.tuigreet to tuigreet

--- hosts/nixos/modules/services/greetd.nix
+++ hosts/nixos/modules/services/greetd.nix
@@ -5,7 +5,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session";
        user = "greeter";
       }; 
     };
