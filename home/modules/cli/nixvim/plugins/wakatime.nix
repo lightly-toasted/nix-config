dbecb531629fb9f7d4c10543d29b301901aa7aa4
lightly-toasted <tooast@duck.com>
2025-10-11 14:51:05 +0900

    feat(nixvim): add .wakatime.cfg with sops-nix for wakatime plugin

--- home/modules/cli/nixvim/plugins/wakatime.nix
+++ home/modules/cli/nixvim/plugins/wakatime.nix
@@ -1,4 +1,10 @@
+{ config, ... }:
+
 {
+  sops.secrets."wakatime" = {
+    path = "${config.home.homeDirectory}/.wakatime.cfg";
+  };
+
   programs.nixvim.plugins.wakatime = {
     enable = true;
     autoLoad = true;
