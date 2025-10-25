868cc1145828b6afcff86ccec734fe1332e489c5
lightly-toasted <tooast@duck.com>
2025-10-25 13:22:00 +0900

    feat(nixos): remove obsidian

--- hosts/nixos/modules/services/flatpak.nix
+++ hosts/nixos/modules/services/flatpak.nix
@@ -6,19 +6,12 @@
     packages = [
       "org.vinegarhq.Sober"
       "org.vinegarhq.Vinegar"
-      "md.obsidian.Obsidian"
     ];
     overrides = {
       "org.vinegarhq.Sober".Context = {
         filesystems = [
           "xdg-run/app/com.discordapp.Discord:create"
           "xdg-run/discord-ipc-0"
-        ];
-      };
-      "md.obsidian.Obsidian".Context = {
-        filesystems = [
-          "home"
-          "/data/Backup/Obsidian"
         ];
       };
     };
