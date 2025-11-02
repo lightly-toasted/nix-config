f6d6f886d5b4ffde88868b058c608fbe616821c1
lightly-toasted <tooast@duck.com>
2025-11-02 13:44:12 +0900

    feat(vps): add Nextcloud as backup destination

--- hosts/vps/modules/services/restic.nix
+++ hosts/vps/modules/services/restic.nix
@@ -1,28 +1,48 @@
 { config, ... }:
 
-{
+let
+  paths = [
+    "/var/lib/zipline"
+    "/var/lib/postgresql"
+    "/var/lib/forgejo"
+    "/var/lib/trilium"
+    "/var/lib/bitwarden_rs"
+  ];
+in {
   sops.secrets = {
     "restic/password" = {};
     "restic/env" = {};
+    "restic/rclone-config" = {};
   };
 
   services.restic.backups.b2 = {
     initialize = true;
     inhibitsSleep = true;
     passwordFile = config.sops.secrets."restic/password".path;
-    paths = [
-      "/var/lib/zipline"
-      "/var/lib/postgresql"
-      "/var/lib/forgejo"
-      "/var/lib/trilium"
-      "/var/lib/bitwarden_rs"
-    ];
+    paths = paths;
     repository = "s3:https://s3.us-east-005.backblazeb2.com/restic-backups-vps";
     environmentFile = config.sops.secrets."restic/env".path;
     pruneOpts = [
       "--keep-daily 7"
       "--keep-weekly 3"
       "--keep-monthly 3"
+    ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+    };
+  };
+  services.restic.backups.nextcloud = {
+    initialize = true;
+    inhibitsSleep = true;
+    passwordFile = config.sops.secrets."restic/password".path;
+    paths = paths;
+    repository = "rclone:nextcloud:restic/vps";
+    rcloneConfigFile = config.sops.secrets."restic/rclone-config".path;
+    pruneOpts = [
+      "--keep-daily 7"
+      "--keep-weekly 4"
+      "--keep-monthly 6"
     ];
     timerConfig = {
       OnCalendar = "daily";
