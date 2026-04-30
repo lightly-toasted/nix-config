282f97f848491dd2848fcff82113015cd6d2592c
lightly-toasted <tooast@duck.com>
2026-05-01 00:02:15 +0900

    chore(home/ideapad): add ssh keys

--- hosts/ideapad/modules/users.nix
+++ hosts/ideapad/modules/users.nix
@@ -5,7 +5,11 @@
     isNormalUser = true;
     description = "toast";
     extraGroups = [ "networkmanager" "wheel" "ydotool" ];
-    packages = with pkgs; [];
+    openssh.authorizedKeys.keys = [
+      ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOyVXtny3ca64wdJAwcUro+U4sY4r6v97ypIXdedOuhc toast@nixos'' 
+      ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ8finfQLaXSqxB16XjsVogN8NRAkNj3Un7JTXlLiLYj u0_a173@localhost''
+      ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOoq1IqrOJ0AAmWRsX0ULiQioEPH9Xo30nwBlrODkude u0_a299@localhost''
+    ];
     shell = pkgs.zsh;
   };
 }
