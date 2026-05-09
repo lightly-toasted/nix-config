7ccdc5e09e46763f65c8163c89b181e73913a012
lightly-toasted <tooast@duck.com>
2026-05-09 22:32:52 +0900

    feat(ideapad): add ssh keys to root

--- hosts/ideapad/modules/users.nix
+++ hosts/ideapad/modules/users.nix
@@ -1,15 +1,19 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
+let
+  authorizedKeys = [
+    ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOyVXtny3ca64wdJAwcUro+U4sY4r6v97ypIXdedOuhc toast@nixos'' 
+    ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ8finfQLaXSqxB16XjsVogN8NRAkNj3Un7JTXlLiLYj u0_a173@localhost''
+    ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOoq1IqrOJ0AAmWRsX0ULiQioEPH9Xo30nwBlrODkude u0_a299@localhost''
+  ];
+in
 {
   users.users.toast = {
     isNormalUser = true;
     description = "toast";
     extraGroups = [ "networkmanager" "wheel" "ydotool" ];
-    openssh.authorizedKeys.keys = [
-      ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOyVXtny3ca64wdJAwcUro+U4sY4r6v97ypIXdedOuhc toast@nixos'' 
-      ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ8finfQLaXSqxB16XjsVogN8NRAkNj3Un7JTXlLiLYj u0_a173@localhost''
-      ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOoq1IqrOJ0AAmWRsX0ULiQioEPH9Xo30nwBlrODkude u0_a299@localhost''
-    ];
+    openssh.authorizedKeys.keys = authorizedKeys;
     shell = pkgs.zsh;
   };
+  users.users.root.openssh.authorizedKeys.keys = authorizedKeys;
 }
