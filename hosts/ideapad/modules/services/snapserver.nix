9286a085338e1ebee59f76722556b12e26edc366
lightly-toasted <tooast@duck.com>
2026-07-17 00:30:46 +0900

    refactor(snapserver): replace pw-cli sink with module-pipe-sink

--- hosts/ideapad/modules/services/snapserver.nix
+++ hosts/ideapad/modules/services/snapserver.nix
@@ -1,56 +1,26 @@
 { pkgs, ... }:
 
 {
-  systemd.user.services.snapcast-sink = {
-    wantedBy = [
-      "pipewire.service"
-    ];
-    after = [
-      "pipewire.service"
-    ];
-    bindsTo = [
-      "pipewire.service"
-    ];
-    path = with pkgs; [
-      pipewire
-    ];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    extraConfig.pipewire-pulse."40-snapcast-sink" = {
+      "pulse.cmd" = [
+        {
+          cmd = "load-module";
+          args = "module-pipe-sink file=/run/snapserver/pipewire sink_name=Snapcast format=s16le rate=48000";
+        }
+      ];
     };
-    script = ''
-      pw-cli create-node adapter '{
-        factory.name=support.null-audio-sink
-        node.name=Snapcast
-        media.class=Audio/Sink
-        object.linger=1
-        audio.position=[FL,FR]
-        monitor.channel-volumes=true
-      }'
-    '';
   };
-      
-  systemd.user.services.snapcast-bridge = {
-    wantedBy = [ "snapcast-sink.service" ];
-    after = [ "snapcast-sink.service" ];
-    requires = [ "snapcast-sink.service" ];
-    path = with pkgs; [ pulseaudio ];
-    serviceConfig = {
-      Type = "simple";
-      Restart = "always";
-      RestartSec = 2;
-    };
-    script = ''
-      while [ ! -p /run/snapserver/pipewire ]; do sleep 1; done
-      parec --device=Snapcast.monitor --format=s16le --rate=44100 --channels=2 --latency=500 > /run/snapserver/pipewire
-    '';
-  };
-
   services.snapserver = {
     settings.stream.source = [
-      "pipe:///run/snapserver/pipewire?name=opus&codec=opus&sampleformat=44100:16:2"
+      "pipe:///run/snapserver/pipewire?name=opus&codec=opus&sampleformat=48000:16:2"
     ];
   };
-
-  systemd.services.snapserver.serviceConfig.ExecStartPost = "${pkgs.coreutils}/bin/chmod 666 /run/snapserver/pipewire";
+  systemd.services.snapserver.serviceConfig = {
+    SupplementaryGroups = [ "audio" ];
+    RuntimeDirectoryMode = "0770";
+    ExecStartPost = "+${pkgs.bash}/bin/bash -c '${pkgs.coreutils}/bin/chown -R snapserver:audio /run/snapserver && ${pkgs.coreutils}/bin/chmod 770 /run/snapserver && ${pkgs.coreutils}/bin/chmod 660 /run/snapserver/pipewire'";
+  };
 }
