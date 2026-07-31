c287381573c50d9cae8553d7ec49703a835825c4
lightly-toasted <tooast@duck.com>
2026-07-31 11:34:44 +0900

    chore(caddy): update hash

--- modules/server/caddy.nix
+++ modules/server/caddy.nix
@@ -6,7 +6,7 @@
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
-      hash = "sha256-bzMqxWTqrJ1skZmRTXyEMCKStXpljbqe5r0Ve2cnBfM=";
+      hash = "sha256-hEHgAG0F0ozHRAPuxEqLyTATBrE+pajeXDiSNwniorg=";
     };
     environmentFile = config.sops.secrets.caddy_env.path;
   };
