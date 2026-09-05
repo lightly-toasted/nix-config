43644f23f15b84fa0ff8ad3f5d90a0ffc68b5d32
lightly-toasted <tooast@duck.com>
2026-09-05 13:56:35 +0900

    refactor(pufferpanel): run via oci-containers

--- modules/server/pufferpanel.nix
+++ modules/server/pufferpanel.nix
@@ -1,3 +1,12 @@
 {
-  services.pufferpanel.enable = true;
+  virtualisation.oci-containers.containers.pufferpanel = {
+    image = "pufferpanel/pufferpanel:latest";
+    ports = [ "8080:8080" "5657:5657" ];
+    volumes = [
+      "/etc/pufferpanel:/etc/pufferpanel"
+      "/var/lib/pufferpanel:/var/lib/pufferpanel"
+      "/var/run/docker.sock:/var/run/docker.sock"
+    ];
+    autoStart = true;
+  };
 }
