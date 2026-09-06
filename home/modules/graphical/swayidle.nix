5aaac4d8b6fed1be881bccc9bac56fe3276a486c
lightly-toasted <tooast@duck.com>
2026-09-06 21:00:00 +0900

    fix(swayidle): use nix store path for niri

--- home/modules/graphical/swayidle.nix
+++ home/modules/graphical/swayidle.nix
@@ -3,13 +3,13 @@
     enable = true;
     events = {
       "before-sleep" = "${pkgs.swaylock}/bin/swaylock -f";
-      "after-resume" = "niri msg action power-on-monitors";
+      "after-resume" = "${pkgs.niri}/bin/niri msg action power-on-monitors";
     };
     timeouts = [
       {
         timeout = 300;
-        command = "niri msg action power-off-monitors";
-        resumeCommand = "niri msg action power-on-monitors";
+        command = "${pkgs.niri}/bin/niri msg action power-off-monitors";
+        resumeCommand = "${pkgs.niri}/bin/niri msg action power-on-monitors";
       }
     ];
   };
